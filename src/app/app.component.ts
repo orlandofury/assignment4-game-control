7bed6d991e9e50d111dc2530c9981f4927fc9776
Orlando Flores Silva <orlandofury@gmail.com>
2023-01-14 20:10:13 -0600

    Assignment 4 Done! property, event binding and view encapsulation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'assignment4-game-control';
+  oddNumbers: number[] = [];
+  evenNumbers: number[] = [];
+
+  onIntervalFired(firedNumber: number) {
+    if(firedNumber % 2 === 0){
+      this.evenNumbers.push(firedNumber);
+    }
+    else{
+      this.oddNumbers.push(firedNumber);
+    }
+  }
 }
